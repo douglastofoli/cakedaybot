635f34385940c8f233af03d5200c4cb14ee7e84a
Douglas <tofoli.douglas@hotmail.com>
2021-01-29 23:07:20 -0300

    fix bug 'is not a constructor'

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Telegraf, Stage, session, WizardScene } from 'telegraf';
+import { Telegraf, session, Scenes } from 'telegraf';
 import LocalSession from 'telegraf-session-local';
 import Calendar from 'telegraf-calendar-telegram';
 import * as dotenv from 'dotenv';
@@ -16,15 +16,40 @@ const bot = new Telegraf(BOT_TOKEN);
 
 const calendar = new Calendar(bot);
 
-bot.use(new LocalSession({ database: 'database.json' }).middleware());
+const property = 'data';
 
-calendar.setDateListener((context, date) => context.reply(date));
+const localSession = new LocalSession({
+  // Database name/path, where sessions will be located (default: 'sessions.json')
+  database: 'database.json',
+  // Name of session property object in Telegraf Context (default: 'session')
+  property: 'session',
+  // Type of lowdb storage (default: 'storageFileSync')
+  storage: LocalSession.storageFileAsync,
+  // Format of storage/database (default: JSON.stringify / JSON.parse)
+  format: {
+    serialize: (obj) => JSON.stringify(obj, null, 2), // null & 2 for pretty-formatted JSON
+    deserialize: (str) => JSON.parse(str)
+  },
+  // We will use `messages` array in our database to store user messages using exported lowdb instance from LocalSession via Telegraf Context
+  state: { users: [] }
+});
 
-const addBirthday = new WizardScene(
+localSession.DB.then((DB) => {
+  // Database now initialized, so now you can retrieve anything you want from it
+  console.log('Current LocalSession DB:', DB.value());
+  // console.log(DB.get('sessions').getById('1:1').value())
+});
+
+bot.use(localSession.middleware(property));
+
+calendar.setDateListener((ctx, date) => {
+  ctx.session.users.birthday = date;
+});
+
+const addBirthday = new Scenes.WizardScene(
   'ADD_BIRTHDAY',
   (ctx) => {
     ctx.reply('Quem vai fazer aniversário?');
-    ctx.session.users = {};
     return ctx.wizard.next();
   },
   (ctx) => {
@@ -33,24 +58,34 @@ const addBirthday = new WizardScene(
       ctx.reply('Esse usuário não é real');
       return;
     }
-    ctx.session.users.username = ctx.message.text;
-    ctx.reply('Qual é a data?', calendar.getCalendar());
+
+    ctx[property + 'DB'].get('users').push([ctx.message.text]).write();
+
+    ctx.reply('Qual o dia? yyyy-mm-dd');
+
     return ctx.wizard.next();
   },
-  async (ctx) => {
-    ctx.session.users.birthday = ctx.message.text;
-    ctx.reply('REGISTRADO');
+  (ctx) => {
+    ctx[property + 'DB'].get('users').push([ctx.message.text]).write();
+    ctx.reply('Adicionado!');
     return ctx.scene.leave();
   }
 );
 
-const stage = new Stage([addBirthday], { default: 'ADD_BIRTHDAY' });
+const listBirthday = new Scenes.WizardScene('LIST_BIRTHDAY', (ctx) => {
+  ctx.reply(ctx[property + 'DB'].get('users'));
+  return ctx.scene.leave();
+});
+
+const stage = new Scenes.Stage([addBirthday, listBirthday]);
 
 bot.use(session());
 bot.use(stage.middleware());
 
-bot.command('add', async (ctx) => {
-  await ctx.scene.enter('ADD_BIRTHDAY');
+bot.command('/add', (ctx) => ctx.scene.enter('ADD_BIRTHDAY'));
+
+bot.command('/listcakeday', (ctx) => {
+  ctx.scene.enter('LIST_BIRTHDAY');
 });
 
 bot.launch();
